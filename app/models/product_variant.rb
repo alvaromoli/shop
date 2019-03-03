cc9b69a31f9210ce27981c692eb82001e877776d
Álvaro Molina <abusmolina@gmail.com>
2019-03-03 12:47:23 -0300

    Mount y migrates

---
+++ app/models/product_variant.rb
@@ -0,0 +1,2 @@
+class ProducVariant < ApplicationRecord
+end
---
+++ test/models/produc_variant_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ProducVariantTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
